ac89eba379867653fc2aad951360bd6408e3690b
ckot <scott.t.silliman@gmail.com>
2024-11-15 02:01:03 -0500

    simply reorganized order of methods so they are grouped logically

--- nnlib/model.py
+++ nnlib/model.py
@@ -22,7 +22,6 @@ class Model:
         self.layers.append(layer)
 
     def set(self, *, loss=None, optimizer=None, accuracy=None):
-
         self.loss = loss
         self.optimizer = optimizer
         self.accuracy = accuracy
@@ -75,6 +74,46 @@ class Model:
                 isinstance(self.loss, Loss_CategoricalCrossentropy):
            self.softmax_classifier_output = \
                Activation_Softmax_Loss_CategoricalCrossentropy()
+
+    # Performs forward pass
+    def forward(self, X, training):
+
+        # Call forward methond on the input layer
+        # this will set the output property that
+        # the first layer in "prev" object is expecting
+        self.input_layer.forward(X, training)
+
+        # Call forward method of every object in a chain
+        # Pass the output of the previous object as a parameter
+        for layer in self.layers:
+            layer.forward(layer.prev.output, training)
+
+        # "layer" is not the last object from list
+        # return its output
+        return layer.output
+
+    # Performs backward pass
+    def backward(self, output, y):
+
+        if self.softmax_classifier_output is not None:
+            self.softmax_classifier_output.backward(output, y)
+
+            self.layers[-1].dinputs = \
+                self.softmax_classifier_output.dinputs
+
+            for layer in reversed(self.layers[:-1]):
+                layer.backward(layer.next.dinputs)
+
+            return
+        # First call backward method on the lost
+        # this will set dinputs property that the last
+        # layer will try to access shortly
+        self.loss.backward(output, y)
+
+        # Call bacward method going thorugh all the objects
+        # in reversed order passing dinputs as parameter
+        for layer in reversed(self.layers):
+            layer.backward(layer.next.dinputs)
 
     # Train the model
     def train(self, X, y, *, epochs=1, batch_size=None,
@@ -172,46 +211,6 @@ class Model:
             if validation_data is not None:
                 self.evaluate(*validation_data, batch_size=batch_size)
 
-    # Performs forward pass
-    def forward(self, X, training):
-
-        # Call forward methond on the input layer
-        # this will set the output property that
-        # the first layer in "prev" object is expecting
-        self.input_layer.forward(X, training)
-
-        # Call forward method of every object in a chain
-        # Pass the output of the previous object as a parameter
-        for layer in self.layers:
-            layer.forward(layer.prev.output, training)
-
-        # "layer" is not the last object from list
-        # return its output
-        return layer.output
-
-    # Performs backward pass
-    def backward(self, output, y):
-
-        if self.softmax_classifier_output is not None:
-            self.softmax_classifier_output.backward(output, y)
-
-            self.layers[-1].dinputs = \
-                self.softmax_classifier_output.dinputs
-
-            for layer in reversed(self.layers[:-1]):
-                layer.backward(layer.next.dinputs)
-
-            return
-        # First call backward method on the lost
-        # this will set dinputs property that the last
-        # layer will try to access shortly
-        self.loss.backward(output, y)
-
-        # Call bacward method going thorugh all the objects
-        # in reversed order passing dinputs as parameter
-        for layer in reversed(self.layers):
-            layer.backward(layer.next.dinputs)
-
     # Evaluates the model using passed-in dataset
     def evaluate(self, X_val, y_val, *, batch_size=None):
 
@@ -224,8 +223,7 @@ class Model:
             if validation_steps * batch_size < len(X_val):
                 validation_steps += 1
 
-        # Reset accumulated values in loss
-        # and accuracy objects
+        # Reset accumulated values in loss and accuracy objects
         self.loss.new_pass()
         self.accuracy.new_pass()
 
@@ -262,6 +260,42 @@ class Model:
         print(f'validation, ' +
               f'acc: {validation_accuracy:.3f}, ' +
               f'loss: {validation_loss:.3f}')
+
+    # Predicts on the samples
+    def predict(self, X, *, batch_size=None):
+
+        # Default value if batch size is not being set
+        prediction_steps = 1
+
+        # Calculate number of steps
+        if batch_size is not None:
+            prediction_steps = len(X) // batch_size
+            if prediction_steps * batch_size < len(X):
+                prediction_steps += 1
+
+        # Model outputs
+        output = []
+
+        # Iterate over steps
+        for step in range(prediction_steps):
+
+            # if batch_size is not set -
+            # use one step and full dataset
+            if batch_size is None:
+                batch_X = X
+
+            # Otherwise slice a batch
+            else:
+                batch_X = X[step*batch_size:(step+1)*batch_size]
+
+            # Perform the forward pass
+            batch_output = self.forward(batch_X, training=False)
+
+            # Append batch predictions to the lsit of predictions
+            output.append(batch_output)
+
+        # Stack and return results
+        return np.vstack(output)
 
     # Retrieves and returns parameters of trainable layers
     def get_parameters(self):
@@ -330,7 +364,6 @@ class Model:
             pickle.dump(model, f)
 
     # Loads and returns a model
-
     @staticmethod
     def load(path):
         # open file in binary-read model, load a model
@@ -340,38 +373,3 @@ class Model:
         # return the loaded model
         return model
 
-    # Predicts on the samples
-    def predict(self, X, *, batch_size=None):
-
-        # Default value if batch size is not being set
-        prediction_steps = 1
-
-        # Calculate number of steps
-        if batch_size is not None:
-            prediction_steps = len(X) // batch_size
-            if prediction_steps * batch_size < len(X):
-                prediction_steps += 1
-
-        # Model outputs
-        output = []
-
-        # Iterate over steps
-        for step in range(prediction_steps):
-
-            # if batch_size is not set -
-            # use one step and full dataset
-            if batch_size is None:
-                batch_X = X
-
-            # Otherwise slice a batch
-            else:
-                batch_X = X[step*batch_size:(step+1)*batch_size]
-
-            # Perform the forward pass
-            batch_output = self.forward(batch_X, training=False)
-
-            # Append batch predictions to the lsit of predictions
-            output.append(batch_output)
-
-        # Stack and return results
-        return np.vstack(output)
