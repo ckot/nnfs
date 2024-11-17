0eb008aed44e17d2c9e81fce26f420f6aef72893
Scott Silliman <scott.t.silliman@gmail.com>
2024-11-16 22:42:03 -0500

    now import DataLoader from nnlib

--- new_load_evaluate.py
+++ new_load_evaluate.py
@@ -1,16 +1,11 @@
 import numpy as np
 
+from nnlib.data import DataLoader
+
 from fashion_mnist_dataset import (
-    DataLoader, FashionMNistDataset, transform_image_data
+    FashionMNistDataset, transform_image_data
 )
 from fashion_mnist_model import fashion_mnist_model as model
-# from nnlib.model import Model
-# from nnlib.layers import Layer_Dense, Layer_Input
-# from nnlib.activation import Activation_ReLU, Activation_Softmax
-# from nnlib.loss import Loss, Loss_CategoricalCrossentropy
-# from nnlib.enhancements import Activation_Softmax_Loss_CategoricalCrossentropy
-# from nnlib.optimizers import Optimizer_Adam
-# from nnlib.accuracy import Accuracy_Categorical
 
 test_dataset = FashionMNistDataset("fashion_mnist/test/labels.csv",
                                    "fashion_mnist/test/images",
