5946bc0802ec191273f5003cb6a6e138b4d1477c
Scott Silliman <scott.t.silliman@gmail.com>
2024-11-16 22:43:56 -0500

    now imports base class from nnlib. removed kruft

--- fashion_mnist_dataset.py
+++ fashion_mnist_dataset.py
@@ -1,4 +1,4 @@
-import functools
+from functools import cache
 import os
 from pathlib import Path
 from typing import Callable, Tuple
@@ -8,41 +8,10 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
-def load_mnist_dataset(dataset, path):
-
-    # Scan all the directories and create a list of labels
-    labels = os.listdir(os.path.join(path, dataset))
-
-    # Create lists for samples and labels
-    X = []
-    y = []
-
-    # for each label folder:
-    for label in tqdm(labels):
-        for file in tqdm(os.listdir(os.path.join(path, dataset, label))):
-            # Read the image
-            pic = Image.open(os.path.join(path, dataset, label, file))
-            pixels = np.array(pic)
-            # And append it and a label to the lists
-            X.append(pixels)
-            y.append(label)
-
-    return np.array(X), np.array(y).astype('uint8')
+from nnlib.data import DataSet
 
 
-def create_data_mnist(path):
-
-    # Load both sets separately
-    print("loading training data")
-    X, y = load_mnist_dataset("train", path)
-    print("loading test data")
-    X_test, y_test = load_mnist_dataset("test", path)
-
-    # And return all the data
-    return X, y, X_test, y_test
-
-
-class FashionMNistDataset:
+class FashionMNistDataset(DataSet):
     def __init__(self,
                  annotations_file: Path | str,
                  images_path: Path | str,
@@ -67,7 +36,7 @@ class FashionMNistDataset:
     def __len__(self):
         return len(self.image_labels)
 
-    @functools.cache
+    @cache
     def __getitem__(self, index: int) -> Tuple[np.array, np.int64] :
         """
         input: int index into image_labels
@@ -108,41 +77,3 @@ def transform_image_data(image: np.array) -> np.array:
     return image
 
 
-class DataLoader:
-    def __init__(self, dataset, *, batch_size=1, shuffle=False, drop_last=True):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.keys = np.arange(0, len(self.dataset))
-        if shuffle:
-            np.random.shuffle(self.keys)
-        self.num_batches = len(dataset) // batch_size
-        if not drop_last and self.num_batches * batch_size < len(self.dataset):
-            self.num_batches += 1
-
-    def __iter__(self):
-        """initializes the iterator, return self as an iterator object"""
-        self.batch_num = 0
-        return self
-
-    def __next__(self):
-        """method which actually performs the iteration"""
-        # keep iterator from return nothing forever when it runs out
-        # of data
-        if self.batch_num >= self.num_batches:
-            raise StopIteration
-        # initialize lists which we'll append values to and convert
-        # to ndarrays later
-        X = []
-        y = []
-        keys = self.keys[self.batch_num * self.batch_size:
-                         (self.batch_num + 1) * self.batch_size]
-        # probably want to remove tqdm, using it currently to both
-        # see what's going on, in particular whether drop_last works
-        # as intended
-        # for key in tqdm(keys):
-        for key in keys:
-            feats, label = self.dataset[key]
-            X.append(feats)
-            y.append(label)
-        self.batch_num += 1
-        return np.array(X), np.array(y)
